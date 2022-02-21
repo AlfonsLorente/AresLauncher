3e8f2989a10087fcfea30e888aca778217b41335
Alfons Lorente Darder <alorente@cifpfbmoll.com>
2022-02-21 18:16:56 +0100

    todo score list view

--- app/src/main/java/com/example/areslauncher/ScoresActivity.java
+++ app/src/main/java/com/example/areslauncher/ScoresActivity.java
@@ -3,12 +3,24 @@ package com.example.areslauncher;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class ScoresActivity extends AppCompatActivity {
+
+    private String[] games;
+    private Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scores);
+        games = getResources().getStringArray(R.array.games);
+        spinner = findViewById(R.id.spinner_score);
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, R.layout.score_spinner_item, games);
+        arrayAdapter.setDropDownViewResource(R.layout.score_spinner_item);
+        spinner.setAdapter(arrayAdapter);
+
+
     }
 }
