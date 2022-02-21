46b13bb1d9954c0d79f8738486b175b59f2b253f
Alfons Lorente Darder <alorente@cifpfbmoll.com>
2022-02-21 17:31:47 +0100

    DOING SCPORE - TODO SPINNER

--- app/src/main/java/com/example/areslauncher/ScoreModel.java
+++ app/src/main/java/com/example/areslauncher/ScoreModel.java
@@ -5,5 +5,45 @@ public class ScoreModel {
     private String user;
     private int highScore;
     private String time;
+    private String game;
 
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public int getHighScore() {
+        return highScore;
+    }
+
+    public void setHighScore(int highScore) {
+        this.highScore = highScore;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getGame() {
+        return game;
+    }
+
+    public void setGame(String game) {
+        this.game = game;
+    }
 }
