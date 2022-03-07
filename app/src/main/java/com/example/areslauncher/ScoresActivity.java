1934b2d57f65e4b19d5bedfe51250312514d5c62
Alfons Lorente <alorente@cifpfbmoll.eu>
2022-03-07 19:52:02 +0100

    Commenting6

--- app/src/main/java/com/example/areslauncher/ScoresActivity.java
+++ app/src/main/java/com/example/areslauncher/ScoresActivity.java
@@ -14,7 +14,11 @@ import android.widget.Spinner;
 
 import java.util.ArrayList;
 
+/**
+ * Show the scores for each game
+ */
 public class ScoresActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    //VARIABLES
     private ArrayList<ScoreModel> scoreModels = new ArrayList<>();
     private ListView listView;
     private String[] games;
@@ -24,11 +28,17 @@ public class ScoresActivity extends AppCompatActivity implements AdapterView.OnI
     private ImageButton back;
     private int actualSpinnerPosition;
 
+    //OVERRIDE METHODS
 
+    /**
+     * Initialize variables and set listeners
+     * @param savedInstanceState Bundle
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scores);
+        //Initialize variables
         dbHelper = new DBHelper(getApplicationContext());
         games = getResources().getStringArray(R.array.games);
         spinner = findViewById(R.id.spinner_score);
@@ -36,11 +46,10 @@ public class ScoresActivity extends AppCompatActivity implements AdapterView.OnI
         arrayAdapter.setDropDownViewResource(R.layout.score_spinner_item);
         spinner.setAdapter(arrayAdapter);
         spinner.setOnItemSelectedListener(this);
-
         back = findViewById(R.id.back_button_score);
-
         listView = findViewById(R.id.listview_score);
 
+        //Listener that closes this activity
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -54,11 +63,19 @@ public class ScoresActivity extends AppCompatActivity implements AdapterView.OnI
     }
 
 
+    /**
+     * When a spinner item is selected show the scores according to the game selected
+     * @param adapterView AdapderView - ?
+     * @param view View
+     * @param position int
+     * @param l long
+     */
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
         MenuActivity.effects.playEffect(R.raw.menu_pick);
-
+        //Save the spinner position
         actualSpinnerPosition = position;
+        //Check for the item picked and fill the scores taking them from the database (Ordered by HIGHSCORE default)
         if (games[position].equalsIgnoreCase("peg")){
             fillPegScoreModels(DBHelper.OrderBy.HIGHSCORE);
 
@@ -69,30 +86,15 @@ public class ScoresActivity extends AppCompatActivity implements AdapterView.OnI
         setUpListViewAdapter();
     }
 
-    private void setUpListViewAdapter() {
-        ScoreAdapter scoreAdapter = new ScoreAdapter(this, scoreModels);
-        listView.setAdapter(scoreAdapter);
-    }
-
 
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
 
     }
 
-
-    private void fillPegScoreModels(DBHelper.OrderBy orderBy) {
-        scoreModels.clear();
-        scoreModels = dbHelper.selectAllUsersPeg(orderBy);
-
-
-    }
-
-    private void fill2048ScoreModels(DBHelper.OrderBy orderBy) {
-        scoreModels.clear();
-        scoreModels = dbHelper.selectAllUsers2048(orderBy);
-    }
-
+    /**
+     * Pause the app and the music
+     */
     @Override
     protected void onPause() {
         if (!activityPressed) {
@@ -104,6 +106,9 @@ public class ScoresActivity extends AppCompatActivity implements AdapterView.OnI
         super.onPause();
     }
 
+    /**
+     * Resume the app and the music
+     */
     @Override
     protected void onResume() {
         MenuActivity.music.resume();
@@ -111,19 +116,24 @@ public class ScoresActivity extends AppCompatActivity implements AdapterView.OnI
     }
 
 
-    //onClick
+    /**
+     * Called by a button onClick, this will order the scores by the button pressed and depending on the game that has been pressed
+     * @param view View
+     */
     public void scoreButtonOrderBy(View view) {
         MenuActivity.effects.playEffect(R.raw.menu_pick);
 
-        Button button = (Button)view;
-        if (button.getText().toString().equalsIgnoreCase(getString(R.string.score_user))) {
-            if (games[actualSpinnerPosition].equalsIgnoreCase("peg")) {
-                fillPegScoreModels(DBHelper.OrderBy.USER);
+        Button button = (Button)view; //set the view as a button
 
-            } else {
-                fill2048ScoreModels(DBHelper.OrderBy.USER);
+        if (button.getText().toString().equalsIgnoreCase(getString(R.string.score_user))) {//if score_user button is pressed
+            if (games[actualSpinnerPosition].equalsIgnoreCase("peg")) {//on game peg
+                fillPegScoreModels(DBHelper.OrderBy.USER);//order by peg
+
+            } else {//on game2048
+                fill2048ScoreModels(DBHelper.OrderBy.USER);//order by user
 
             }
+        //AND SO ON LIKE THE IF BUT WITH THE OTHER BUTTONS
         }else if(button.getText().toString().equalsIgnoreCase(getString(R.string.score_highscore))){
             if (games[actualSpinnerPosition].equalsIgnoreCase("peg")) {
                 fillPegScoreModels(DBHelper.OrderBy.HIGHSCORE);
@@ -144,7 +154,41 @@ public class ScoresActivity extends AppCompatActivity implements AdapterView.OnI
 
             }
         }
+        //Update the list view
         setUpListViewAdapter();
 
     }
+
+    /**
+     * Update/create the score adapter with the scoreModels list and set it to the listView
+     */
+    private void setUpListViewAdapter() {
+        ScoreAdapter scoreAdapter = new ScoreAdapter(this, scoreModels);
+        listView.setAdapter(scoreAdapter);
+    }
+
+    /**
+     * Fill scoreModels list with peg scores
+     * @param orderBy DBHelper.OrderBy
+     */
+    private void fillPegScoreModels(DBHelper.OrderBy orderBy) {
+        scoreModels.clear();
+        scoreModels = dbHelper.selectAllUsersPeg(orderBy);
+
+
+    }
+
+    /**
+     * Fill scoreModels list with 2048 scores
+     * @param orderBy DBHelper.OrderBy
+     */
+    private void fill2048ScoreModels(DBHelper.OrderBy orderBy) {
+        scoreModels.clear();
+        scoreModels = dbHelper.selectAllUsers2048(orderBy);
+    }
+
+
+
+
+
 }
